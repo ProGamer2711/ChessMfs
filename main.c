b1063bc40626383551ea7453ec4af8fa6f6459d0
ProGamer2711 <alexandurshestakov@gmail.com>
2024-06-10 19:25:03 +0300

    added some macros and typedefs

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define EMPTY_SPACE 0
+#define WHITE_KING 1
+#define WHITE_ROOK 2
+#define BLACK_KING 3
+
+typedef uint8_t byte;
 
 int main() {
-    printf("Hello, World!\n");
-
     return 0;
 }
