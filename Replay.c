a2a52a1d89a834213d4d47314256f931b0f8b7a5
Skele-ton <nathanielnechevski@gmail.com>
2024-06-15 18:03:03 +0300

    Update writing to file

--- Replay.c
+++ Replay.c
@@ -8,11 +8,37 @@
 #include "Menu.h"
 #include "Vector.h"
 
+#define MOVE_SIZE 5
+
 // 1 byte for board size
 // 1 byte for piece count
 // 2 bytes for each piece's position
 byte calculateSeedLength(byte pieceCount) {
     return 2 + pieceCount * 2;
+}
+
+void writeMoveToFile(FILE *file, Move *move) {
+    PieceType pieceTakenType = move->pieceTaken == NULL ? 0 : move->pieceTaken->type;
+    byte pieceTaken = pieceTakenType;
+
+    // a move can be represented by 5 bytes
+    // 2 bytes for the start position
+    // 2 bytes for the end position
+    // 1 byte for the piece taken (the byte 0 if no piece was taken)
+    byte moveBytes[MOVE_SIZE] = {
+        move->start.x,
+        move->start.y,
+        move->end.x,
+        move->end.y,
+        pieceTaken,
+    };
+
+    if (fwrite(moveBytes, sizeof(byte), MOVE_SIZE, file) != MOVE_SIZE) {
+        printf("Failed to write move to file");
+        fclose(file);
+
+        exit(1);
+    }
 }
 
 void writeReplayToFile(byte *seed, size_t seedLength, Vector *moves) {
@@ -41,11 +67,8 @@ void writeReplayToFile(byte *seed, size_t seedLength, Vector *moves) {
         exit(1);
     }
 
-    if (fwrite(moves->data, sizeof(Move), moves->length, file) != moves->length) {
-        printf("Failed to write move to file");
-        fclose(file);
-
-        exit(1);
+    for (byte i = 0; i < moves->length; i++) {
+        writeMoveToFile(file, moves->get(moves, i));
     }
 
     fclose(file);
